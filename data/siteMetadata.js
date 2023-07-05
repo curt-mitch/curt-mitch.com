d6d09477c4c6e390ab294fee23c3542d35cb9a76
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2023-07-05 16:00:39 -0700

    Update icons, about page, and metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,9 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Curt-Mitch.com',
+  author: 'Curtis Mitchell',
+  headerTitle: 'Curt-Mitch.com',
+  description:
+    'A personal blog at the intersection of machine learning, privacy, and mathematics with a dash of web development',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
@@ -10,12 +11,10 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'curtis.l.mitchell@gmail.com',
+  twitter: 'https://twitter.com/curt-mitch',
+  linkedin: 'https://www.linkedin.com/in/curtislmitchell/',
+  github: 'https://github.com/curt-mitch',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
