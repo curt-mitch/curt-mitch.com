56199187cf41479a4df63383898d216dee980979
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2023-08-01 15:16:08 -0700

    Simplify CSP

--- next.config.js
+++ next.config.js
@@ -6,17 +6,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
-  default-src 'self' *.plausible.io;
-  default-src 'self';
-  style-src 'self' 'unsafe-inline';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' plausible.io giscus.app;
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
   font-src 'self';
   frame-src giscus.app
 `
-// removed giscus content
-//   script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
 
 const securityHeaders = [
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
